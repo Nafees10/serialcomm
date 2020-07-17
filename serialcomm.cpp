667c502f772fa5af54cbc2d7f275b2332f5739b5
Nafees10 <nafeeshassan16@gmail.com>
2020-07-17 22:44:14 +0500

    Adds send(), update(), and delay()

--- serialcomm.cpp
+++ serialcomm.cpp
@@ -1,17 +1,9 @@
 #include "Arduino.h"
 
-#ifndef USE_HARDWARE_SERIAL
- 	#include <SoftwareSerial.h>
-#endif
-
 #define HS_RQ_CONNECT 0xF0
 #define HS_RP_CONNECT 0xF1
 #define RQ_VAR 0xF2
 #define RP_VAR 0xF3
-#define RQ_ARR 0xF4
-#define RP_ARR 0xF5
-#define RQ_PING 0xF6
-#define RP_PING 0xF7
 
 #define TIMEOUT_MSECS 1000
 
@@ -24,11 +16,7 @@ struct VarStore{
 /// Can handle upto 241 variables. Do not register more than that
 class SerialComm{
 private:
-	#if defined(USE_HARDWARE_SERIAL)
-		HardwareSerial *_serial;
-	#else
-		SoftwareSerial *_serial;
-	#endif
+	Stream *_serial;
 	VarStore *_vars;
 	uint8_t _varsCount;
 	uint8_t _varsUsedCount;
@@ -39,21 +27,13 @@ private:
 		_varsUsedCount ++;
 	}
 public:
-	#ifdef USE_HARDWARE_SERIAL
-		SerialComm(HardwareSerial &serial, uint8_t varCount)){
-			_serial = serial;
-			_varsCount = varCount;
-			_varsUsedCount = 0;
-			_vars = new VarStore[_varsCount];
-		}
-	#else
-		SerialComm(SoftwareSerial &serial, uint8_t varCount){
-			_serial = serial;
-			_varsCount = varCount;
-			_varsUsedCount = 0;
-			_vars = new VarStore[_varsCount];
-		}
-	#endif
+	SerialComm(Stream &serial, uint8_t varCount){
+		_serial = serial;
+		_serial->setTimeout(TIMEOUT_MSECS);
+		_varsCount = varCount;
+		_varsUsedCount = 0;
+		_vars = new VarStore[_varsCount];
+	}
 	/// registers a variable
 	/// 
 	/// Returns: ID if successful, varCount if failed
@@ -88,7 +68,55 @@ public:
 	/// Sends a single variable, using the variable ID
 	void send(uint8_t id){
 		if (id < _varsCount){
-			
+			_serial->write(RP_VAR);
+			_serial->write(id);
+			_serial->write(_vars[id].length);
+			_serial->write(_vars[id].ptr, _vars[id].length);
+		}
+	}
+	/// sends all registered variables
+	void send(){
+		for (uint8_t i = 0; i < _varsCount; i ++){
+			_serial->write(RP_VAR);
+			_serial->write(i);
+			_serial->write(_vars[i].length);
+			_serial->write(_vars[i].ptr, _vars[i].length);
+		}
+	}
+	/// waits till timeout for commands, or new values, responds if necessary, & 
+	/// updates local values
+	void update(uint8_t timeout){
+		unsigned long t = millis();
+		while (!_serial->available()){
+			if (millis() - t >= timeout)
+				break;
+		}
+		while (_serial->available()){
+			uint8_t command, id, length, *buffer;
+			command = _serial->read();
+			if (command == RQ_VAR){
+				// read the id
+				while (!_serial->available()){}
+				id = _serial->read();
+				this->send(id);
+			}else if (command == RP_VAR){
+				// store this var
+				while (!_serial->available()){}
+				id = _serial->read();
+				while (!_serial->available()){}
+				length = _serial->read();
+				buffer = new uint8_t[length];
+				_serial->readBytes(buffer, length);
+			}
+		}
+	}
+	/// use this instead of regular delay() when timeout is > 100msecs
+	void delay(uint8_t timeout){
+		unsigned long t = millis();
+		update(timeout);
+		if (millis() - t < timeout){
+			unsigned long delayPeriod = timeout - (millis() - t);
+			delay(delayPeriod > timeout ? 0 : delayPeriod);
 		}
 	}
 };
