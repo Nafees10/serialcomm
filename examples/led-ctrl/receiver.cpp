303ac06c9c1883cbe5db1a666a1c9f63bd82f7d2
Nafees10 <nafeeshassan16@gmail.com>
2020-07-20 17:42:38 +0500

    Adds example code to show how to control LED

---
+++ examples/led-ctrl/receiver.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include "serialcomm.h"
+
+#define LEDPIN 13
+
+SerialComm<HardwareSerial, 3> net(&Serial);
+bool ledStatus;
+
+void setup() {
+	pinMode(LEDPIN, OUTPUT);
+	Serial.begin(9600);
+	net.registerVar(&ledStatus, sizeof(ledStatus));
+	net.awaitConnection(); // wait for other arduino to be on before going further
+}
+
+void loop() {
+	digitalWrite(LEDPIN, ledStatus ? HIGH : LOW);
+	net.sleep(250);
+}
---
+++ examples/led-ctrl/sender.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include "serialcomm.h"
+
+#define BUTTONPIN 4
+#define RX 2
+#define TX 3
+
+SoftwareSerial swSerial(RX, TX);
+SerialComm<SoftwareSerial, 3> net(&swSerial);
+bool ledStatus;
+
+void setup() {
+	pinMode(BUTTONPIN, INPUT_PULLUP);
+	Serial.begin(9600);
+	net.registerVar(&ledStatus, sizeof(ledStatus), true); // true sets it to auto-send every time net.sleep or net.update called
+	net.awaitConnection(); // wait for other arduino to be on before going further
+}
+
+void loop() {
+	ledStatus = digitalRead(BUTTONPIN) == HIGH ? true : false;
+	net.sleep(250);
+}
