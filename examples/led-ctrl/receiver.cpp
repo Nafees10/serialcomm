95300c49e3a0259f5857c569c1a5200de65eb355
Nafees10 <nafeeshassan16@gmail.com>
2020-07-21 21:03:53 +0500

    Updates examples to use HardwareSerial

--- examples/led-ctrl/receiver.cpp
+++ examples/led-ctrl/receiver.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
 #include "serialcomm.h"
-#include "SoftwareSerial.h"
 
 #define LEDPIN 13
-#define RX 2
-#define TX 3
 
-SoftwareSerial swSerial(RX, TX);
-SerialComm<SoftwareSerial, 3> net(&swSerial);
+SerialComm<HardwareSerial, 1> net(&Serial);
 bool ledStatus;
 
 void setup() {
 	pinMode(LEDPIN, OUTPUT);
-	swSerial.begin(9600);
+	Serial.begin(9600);
 	net.registerVar(&ledStatus, sizeof(ledStatus));
 	net.awaitConnection(); // wait for other arduino to be on before going further
 }
--- examples/led-ctrl/sender.cpp
+++ examples/led-ctrl/sender.cpp
@@ -1,23 +1,17 @@
 #include <Arduino.h>
 #include "serialcomm.h"
-#include "SoftwareSerial.h"
 
-#define BUTTONPIN 4
-#define RX 2
-#define TX 3
-
-SoftwareSerial swSerial(RX, TX);
-SerialComm<SoftwareSerial, 3> net(&swSerial);
+SerialComm<HardwareSerial, 1> net(&Serial);
 bool ledStatus;
 
 void setup() {
 	pinMode(BUTTONPIN, INPUT_PULLUP);
-	swSerial.begin(9600);
+	Serial.begin(9600);
 	net.registerVar(&ledStatus, sizeof(ledStatus), true); // true sets it to auto-send every time net.sleep or net.update called
 	net.awaitConnection(); // wait for other arduino to be on before going further
 }
 
 void loop() {
-	ledStatus = digitalRead(BUTTONPIN) == HIGH ? true : false;
+	ledStatus = !ledStatus;
 	net.sleep(250);
 }
